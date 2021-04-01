e5ec78faa333b2c8948a04e17fd501c10351f26c
z-shun <2833387545@qq.com>
2021-04-01 14:07:56 +0800

    2021-4-1

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="nav-bar">
+
+  <div class="left"><slot></slot></div>
+  <div class="center"><slot></slot></div>
+  <div class="right"><slot></slot></div>
+
+</div>
+</template>
+
+<script>
+  export default {
+    name:"NavBar"
+  }
+</script>
+
+<style>
+ .nav-bar {
+   display: flex;
+   height: 44px;
+   line-height: 44px;
+ }
+  .left, .right {
+    width: 60px;
+    background-color: red;
+  }
+  .center {
+    flex: 1;
+    background-color: blue;
+  }
+</style>
--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -1,38 +1,18 @@
 <template>
   <div>
-    <nav-bar class="nav-bar"><div slot="center">购物车({{cartLength}})</div></nav-bar>
-    <cart-list :cart-list="cartList"/>
-    <cart-bottom-bar/>
+    cart
   </div>
 </template>
-
 <script>
-  import NavBar from 'components/common/navbar/NavBar'
-  import CartList from './childComps/CartList'
-  import CartBottomBar from './childComps/CartBottomBar'
-
-  import { mapGetters } from 'vuex'
-
   export default {
     name: "Cart",
-    components: {
-    	NavBar,
-      CartList,
-      CartBottomBar
-    },
-    computed: {
-      ...mapGetters([
-      	'cartLength',
-        'cartList'
-      ])
-    }
+
   }
 </script>
-
 <style scoped>
-  .nav-bar {
-    background-color: var(--color-tint);
-    color: #fff;
-    font-weight: 700;
-  }
+  /*.nav-bar {*/
+  /*  background-color: var(--color-tint);*/
+  /*  color: #fff;*/
+  /*  font-weight: 700;*/
+  /*}*/
 </style>
